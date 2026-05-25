2d54d5acb97fb3d06aad4bbe7925f55556164e64
wmzy <1256573276@qq.com>
2026-05-26 05:42:08 +0800

    feat: remove date field from all poems

--- src/content.config.ts
+++ src/content.config.ts
@@ -6,7 +6,6 @@ const poems = defineCollection({
   loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: './src/data/poems' }),
   schema: z.object({
     title: z.string(),
-    date: z.coerce.date(),
     excerpt: z.string().optional(),
     tags: z.array(z.string()).default([]),
   }),
